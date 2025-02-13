77e51c25bd994c6a592fa636140ef66ba245402f
Khushal <khushalr010@gmail.com>
2025-02-13 10:13:11 +0530

    Add file

---
+++ script.js
@@ -0,0 +1,68 @@
+let access_btn = document.querySelectorAll(".btn");
+let access_rbtn = document.querySelector(".reset_btn");
+let access_msgcontainer = document.querySelector(".msg-container"); // Fixed typo
+let access_restart_btn = document.querySelector(".restart_btn");
+let access_msg = document.querySelector("#msg");
+
+let clickO = true;
+const winpatten = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+];
+
+const reset_btn = () => {
+    clickO = true;
+    enable_btn();
+    access_msgcontainer.classList.add("hide");
+};
+
+access_btn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        if (clickO) {
+            btn.innerText = "O";
+            btn.style.color = "red";
+            clickO = false;
+        } else {
+            btn.innerText = "X";
+            btn.style.color = "black";
+            clickO = true;
+        }
+        btn.disabled = true;
+
+        check_winner(); // Fixed function name
+    });
+});
+
+const disable_btn = () => {
+    access_btn.forEach(btn => btn.disabled = true);
+};
+
+const enable_btn = () => {
+    access_btn.forEach(btn => {
+        btn.disabled = false;
+        btn.innerText = "";
+    });
+};
+
+const showwinner = (winner) => {
+    access_msg.innerText = `Congratulations, Winner is ${winner}`;
+    access_msgcontainer.classList.remove("hide");
+    disable_btn();
+};
+
+const check_winner = () => {
+    for (const pattern of winpatten) {
+        let p1 = access_btn[pattern[0]].innerText;
+        let p2 = access_btn[pattern[1]].innerText;
+        let p3 = access_btn[pattern[2]].innerText;
+
+        if (p1 !== "" && p1 === p2 && p2 === p3) {
+            console.log("Winner:", p1);
+            showwinner(p1);
+        }
+    }
+};
+
+access_restart_btn.addEventListener("click", reset_btn);
+access_rbtn.addEventListener("click", reset_btn);
